0a64e2f2ee58cd984c9a949aa3f880156ae3b3b9
YoAquinjs <sebascertuche@gmail.com>
2025-04-22 00:46:49 -0600

    update and document vector interface

--- include/dsa/ds/vector.h
+++ include/dsa/ds/vector.h
@@ -1,74 +1,237 @@
-/*
- * vector data structure
- * the vector data structure provides a dynamic array
- * */
+/**
+ * \file dsa/ds/vector.h
+ * \brief vector data structure header.
+ */
 
 #ifndef DSA_DS_VECTOR_H
 #define DSA_DS_VECTOR_H
 
+#include "dsa/ds/array.h"
 #include "dsa/types/allocator.h"
-#include "dsa/types/array.h"
 #include "dsa/types/growth.h"
 
 #include <stdbool.h>
 
+/**
+ * \brief Vector data structure.
+ *
+ * A vector is a dynamic array backed by an underlying array. It can be resized
+ * manually or automatically using a specified growth strategy.
+ */
 struct vector;
 
+/**
+ * \brief Null vector constant.
+ */
 extern const struct vector NULL_VECTOR;
 
-// construct new vector structure with stdlib allocator
+/**
+ * \brief Create new vector.
+ *
+ * Allocates array of size (size * memb_size) bytes plus memb_buffer of size
+ * memb_size bytes with the stdlib allocator, and initializes vector capacity to
+ * size and vector data as empty (size = 0).
+ *
+ * \param size Initial capacity of the vector.
+ * \param memb_size Vector member size.
+ * \param growth Resizing growth function.
+ *
+ * \return Created vector, or NULL_VECTOR if the allocation fails or any
+ * parameter is invalid.
+ */
 struct vector vector_new(size_t size, size_t memb_size, Growth growth);
-// construct new vector structure with given allocator
-struct vector vector_new_with_allocator(size_t size, size_t memb_size,
-                                        Growth growth,
-                                        const struct allocator* alloc);
-// free vector underlying buffers and null given vector
+
+/**
+ * \brief Create new vector with given allocator.
+ *
+ * Allocates array of size (size * memb_size) bytes plus memb_buffer of size
+ * memb_size bytes with the given allocator, and initializes vector capacity to
+ * size and vector data as empty (size = 0).
+ *
+ * \param size Initial capacity of the vector.
+ * \param memb_size Container member size.
+ * \param growth Resizing growth function.
+ * \param alloc Allocator.
+ *
+ * \return Created vector, or NULL_VECTOR if the allocation fails or any
+ * parameter is invalid.
+ */
+struct vector vector_new_with_alloc(size_t size, size_t memb_size,
+                                    Growth growth,
+                                    const struct allocator* alloc);
+
+/**
+ * \brief Free vector.
+
+ * Does nothing if the vector is null, overwrites vector to null after freeing
+ it.
+ *
+ * \param vec Vector.
+ */
 void vector_free(struct vector* vec);
 
-// construct new vector from array and null given array
-struct vector vector_from_array(struct array* arr, Growth growth,
-                                const struct allocator* alloc);
-// returns pointer to vector underlying array
+/**
+ * \brief Evaluate nullness of vector.
+ *
+ * \param vec Vector.
+ *
+ * \return True if the vector is NULL, false otherwise.
+ */
+bool vector_is_null(const struct vector* vec);
+
+/**
+ * \brief Create new vector from \ref array.
+ *
+ * overwrites arr to null after vector creation.
+ *
+ * \param arr Array.
+ * \param growth Resizing growth function.
+ *
+ * \return Created vector, or NULL_VECTOR if any parameter is invalid.
+ */
+struct vector vector_from_array(struct array* arr, Growth growth);
+
+/**
+ * \brief Get vector's underlying array.
+ *
+ * \param vec Vector.
+ *
+ * \return Constant reference to vector's underlying array, or NULL if the
+ * vector is null.
+ */
 const struct array* vector_to_array(struct vector* vec);
 
-// index element in vector
-// return NULL if out of bounds
+/**
+ * \brief Index element in vector.
+ *
+ * \param vec Vector.
+ * \param idx Index.
+ *
+ * \return Pointer to element at index or NULL if the vector is NULL or
+ * index out of bounds.
+ */
 void* vector_index(const struct vector* vec, size_t idx);
 
-// swap two elements in vector
+/**
+ * \brief Swap elements at indices in vector.
+ *
+ * Elements are swapped by value.
+ *
+ * \param vec Vector.
+ * \param idx_a Index a.
+ * \param idx_b Index b.
+ *
+ * \retval 0 Swap succeeded.
+ * \retval 1 Null vector.
+ * \retval 2 Index out of bounds.
+ */
 int vector_swap(struct vector* vec, size_t idx_a, size_t idx_b);
 
-// insert element at given index in vector displacing remaining elements
-// if capacity if overflown, capacity is reallocated to growth(capacity)
-int vector_insert(struct vector* vec, void* elem, size_t idx);
+/**
+ * \brief Insert element at index in vector.
+ *
+ * Remaining elements are displaced to the right, if the vector is full,
+ * capacity grows by growth strategy.
+ *
+ * \param vec Vector.
+ * \param elem Pointer to elem of size memb_size.
+ * \param idx Index.
+ *
+ * \retval 0 Insert succeeded.
+ * \retval 1 Null vector.
+ * \retval 2 Index out of bounds.
+ * \retval 3 Reallocation failed.
+ * \retval 4 Null elem.
+ */
+int vector_insert(struct vector* vec, const void* elem, size_t idx);
 
-// remove element at index given index in vector displacing remaining elements
-// never frees memory
+/**
+ * \brief Remove element at index in vector.
+ *
+ * Remaining elements are displaced to the left, capacity remains the same.
+ *
+ * \param vec Vector.
+ * \param idx Index.
+
+ * \retval 0 Remove succeeded.
+ * \retval 1 Null vector.
+ * \retval 2 Index out of bounds.
+ */
 int vector_remove(struct vector* vec, size_t idx);
 
-// append element to vector
-int vector_append(struct vector* vec, void* elem);
+/**
+ * \brief Append element after last index to vector.
+ *
+ * If the vector is full, capacity grows by growth strategy.
+ *
+ * \param vec Vector.
+ * \param elem Pointer to elem of size memb_size.
+ *
+ * \retval 0 Append succeeded.
+ * \retval 1 Null vector
+ * \retval 3 Reallocation failed.
+ * \retval 4 Null elem.
+ */
+int vector_append(struct vector* vec, const void* elem);
 
-// remove last element from vector
+/**
+ * \brief Pop last element from vector.
+ *
+ * Fails if the vector is empty, capacity remains the same.
+ *
+ * \param vec Vector.
+ *
+ * \retval 0 Pop succeeded.
+ * \retval 1 Null vector
+ * \retval 2 Empty vector
+ */
 int vector_pop(struct vector* vec);
 
-// check if size would overflow vector underlying array
-extern inline bool vector_overflow(struct vector* vec, size_t size);
+/**
+ * \brief Evaluate if size is greater than vector's capacity.
+ *
+ * \param size Test size.
+ *
+ * \return True if the vector is null or the size is greater than the vector's
+ * capacity, false otherwise.
+ */
+bool vector_overflow(struct vector* vec, size_t size);
 
-// resizes the vector to given size, reallocates capacity by Growth function
-// truncates elements if given size is less than current
-// new elements created have garbage values
-int vector_resize(struct vector* vec, size_t size);
-// reserves the given capacity reallocating underlying array if needed
-// truncates elements if given capacity is less than current
-// 0 capacity is invalid
-int vector_reserve(struct vector* vec, size_t capacity);
+/**
+ * \brief Set vector size.
+ *
+ * If the new size overflows the capacity, capacity grows by growth strategy,
+ * if the new size is less than current, the size is decreased to the new size,
+ * but truncated elements remain unchanged in the underlying array.
+ *
+ * \param size New size.
+ *
+ * \retval 0 Size set succeeded.
+ * \retval 1 Null vector.
+ * \retval 2 Reallocation failed.
+ */
+int vector_set_size(struct vector* vec, size_t size);
+
+/**
+ * \brief Set vector capacity.
+ *
+ * Reallocates array exactly to the new capacity, if size is greater than the
+ * new capacity, size is set to new the new capacity and remaining elements are
+ * freed, if the given capacity is 0, an error is returned.
+ *
+ * \param capacity New capacity.
+ *
+ * \retval 0 Capacity set succeeded.
+ * \retval 1 Null vector.
+ * \retval 2 Reallocation failed.
+ * \retval 3 Invalid new capacity of value 0.
+ */
+int vector_set_capacity(struct vector* vec, size_t capacity);
 
 struct vector {
     size_t size;
     void* memb_buffer;
     const Growth growth;
-    const struct allocator* const alloc;
     struct array arr;
 };
 
