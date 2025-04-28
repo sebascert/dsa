137a4c6bd68030db0493941e796bc87c2790b156
Sebastian Certuche <sebascertuche@gmail.com>
2025-04-28 17:16:20 -0600

    add test for growth strategy utils

---
+++ test/utils/growth.c
@@ -0,0 +1,36 @@
+#include "dsa/utils/growth.h"
+
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include <criterion/parameterized.h>
+
+#define suite utils_growth
+
+struct growth_tc {
+    size_t size;
+    size_t exp;
+    size_t exp_m;
+    size_t bin_tree;
+    size_t next_bin_tree;
+};
+
+ParameterizedTestParameters(suite, strategies)
+{
+    static struct growth_tc params[] = {
+        {0, 0, 0, 1, 1},    {1, 2, 3, 3, 3},      {2, 4, 6, 5, 3},
+        {5, 10, 15, 11, 7}, {10, 20, 30, 21, 15}, {15, 30, 45, 31, 31},
+    };
+    return cr_make_param_array(struct growth_tc, params,
+                               sizeof(params) / sizeof(struct growth_tc));
+}
+
+ParameterizedTest(struct growth_tc* param, suite, strategies)
+{
+    cr_assert(eq(sz, exponential_growth(param->size), param->exp));
+    cr_assert(eq(sz, exponential_m_growth(param->size), param->exp_m));
+    cr_assert(eq(sz, complete_bintree_growth(param->size), param->bin_tree));
+    cr_assert(eq(sz, next_complete_bintree_growth(param->size),
+                 param->next_bin_tree));
+}
+
+#undef suite
